b60052cc6b0f63f42e72d2c484f458e52dcb4e1a
axert5 <36628670+axert5@users.noreply.github.com>
2018-03-14 11:46:51 +0100

    renamed

--- rho.py
+++ rho.py
@@ -6,27 +6,20 @@ Created on Wed Mar 14 09:26:07 2018
 """
 
 from numpy import*
-
-p0=101325
-T0=288.15
-lamda=-0.0065
-g=9.80665
-R=287.05
-gamma=1.4
-rho0=1.225
-cp=gamma*R/(gamma-1)
+from Cit_par import *
 
 
-def rho0(hp):
 
-    p=(1+lamda*hp/T0)**(-g/(lamda*R))*p0
+def rho00(hp):
+
+    p=(1+Lambda*hp/Temp0)**(-g/(Lambda*R))*p0
 
     rho=(p/p0)**(1/gamma)*rho0
     return rho
 
-def rho(hp,Vc,Ttot): 
+def rho1(hp,Vc,Ttot): 
     T=Ttot-Vc**2/(2*cp)
-    p=(1+lamda*hp/T0)**(-g/(lamda*R))*p0
+    p=(1+Lambda*hp/Temp0)**(-g/(Lambda*R))*p0
     
     rho=p/(R*T)
     return rho
