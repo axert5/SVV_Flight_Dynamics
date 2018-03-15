829ef171838d1eafee0b7715bf264f90f238c979
andubadea <37139260+andubadea@users.noreply.github.com>
2018-03-15 10:24:49 +0100

    asd

--- Analytical_sim.py
+++ Analytical_sim.py
@@ -1,20 +1,49 @@
 from Cit_par import *
 from EigFuncVer import *
+import numpy as np
 
 
-
-print("Dimension-less for short period are:" , short_period(muc , KY2 , CZa , Cmadot, Cmq, Cma, V0 , c ))
+'''
+print("Dimension-less for short period are:" , short_period(muc , KY2 , CZa , Cmadot, Cmq, Cma))
 print()
-print("Dimension-less for phugoid are:" , phugoid(muc, CZa, Cmq, Cma,CXu , Cmu, CXa, CZu , CZ0 ,V0 , c))
+print("Dimension-less for phugoid are:" , phugoid(muc, CZa, Cmq, Cma,CXu , Cmu, CXa, CZu , CZ0))
 print()
-print("Dimension-less for Dutch roll are:" , dutch_roll(mub , KZ2 , Cnr , CYb , Cnb, V0 , b))
+print("Dimension-less for Dutch roll are:" , dutch_roll(mub , KZ2 , Cnr , CYb , Cnb))
 print ("-----------------------------------------------")
 print("Dimension-having eigenvalues for short period are:" , short_period_d(muc , KY2 , CZa , Cmadot, Cmq, Cma , V0 , c))
 print()
 print("Dimension-having eigenvalues for phugoid are:" , phugoid_d(muc, CZa, Cmq, Cma,CXu , Cmu, CXa, CZu , CZ0, V0 , c))
 print()
 print("Dimension-having eigenvalues for Dutch roll are:" , dutch_roll_d(mub , KZ2 , Cnr , CYb , Cnb , V0 , b))
+'''
 
+#Taking real parts of each dimensionless eigenvalue
+Re = np.array([[np.real(short_period(muc , KY2 , CZa , Cmadot, Cmq, Cma)[0])] ,
+                [np.real(phugoid(muc, CZa, Cmq, Cma,CXu , Cmu, CXa, CZu , CZ0)[0])],
+                [np.real(dutch_roll(mub , KZ2 , Cnr , CYb , Cnb)[0])]])
 
+#Imaginary parts of each dimensionless eigenvalue
+Im = np.array([[np.imag(short_period(muc , KY2 , CZa , Cmadot, Cmq, Cma)[0])] ,
+                [np.imag(phugoid(muc, CZa, Cmq, Cma,CXu , Cmu, CXa, CZu , CZ0)[0])],
+                [np.imag(dutch_roll(mub , KZ2 , Cnr , CYb , Cnb)[0])]])
 
+#Short period data
+P_sp       = 2*pi*c/(V0 * Im[0,0])                         #Period
+T_half_sp  = np.log(0.5) * c / (V0 * Re[0,0])              #Half amplitude time
+omega0_sp  = np.sqrt(Re[0,0]**2 + Im[0,0]**2) * V0 / c     #Angular frequency of oscillation
+damp_sp    = -Re[0,0] / (np.sqrt(Re[0,0]**2 + Im[0,0]**2)) #Damping coefficient
+omegan_sp  = omega0_sp * np.sqrt(1-damp_sp**2)                   #Eigenfrequency 
 
+#Phugoid data
+P_ph       = 2*pi*c/(V0 * Im[1,0])                         #Period
+T_half_ph  = np.log(0.5) * c / (V0 * Re[1,0])              #Half amplitude time
+omega0_ph  = np.sqrt(Re[1,0]**2 + Im[1,0]**2) * V0 / c     #Angular frequency of oscillation
+damp_ph    = -Re[1,0] / (np.sqrt(Re[1,0]**2 + Im[1,0]**2)) #Damping coefficient
+omegan_ph  = omega0_ph * np.sqrt(1-damp_ph**2)                   #Eigenfrequency 
+
+#Dutch roll data
+P_dr       = 2*pi*b/(V0 * Im[2,0])                         #Period
+T_half_dr  = np.log(0.5) * b / (V0 * Re[2,0])              #Half amplitude time
+omega0_dr  = np.sqrt(Re[2,0]**2 + Im[2,0]**2) * V0 / b     #Angular frequency of oscillation
+damp_dr    = -Re[2,0] / (np.sqrt(Re[2,0]**2 + Im[2,0]**2)) #Damping coefficient
+omegan_dr  = omega0_dr * np.sqrt(1-damp_dr**2)                   #Eigenfrequency 
