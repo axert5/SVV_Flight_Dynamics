2df53a05472bca20f5397cd183c42983f8407365
andubadea <37139260+andubadea@users.noreply.github.com>
2018-03-15 08:55:41 +0100

    not printing shit anymore

--- Cit_par.py
+++ Cit_par.py
@@ -116,16 +116,4 @@ Cnda   =  -0.0120
 Cndr   =  -0.0939
 
 
-print("Dimension-less for short period are:" , short_period(muc , KY2 , CZa , Cmadot, Cmq, Cma, V0 , c ))
-print()
-print("Dimension-less for phugoid are:" , phugoid(muc, CZa, Cmq, Cma,CXu , Cmu, CXa, CZu , CZ0 ,V0 , c))
-print()
-print("Dimension-less for Dutch roll are:" , dutch_roll(mub , KZ2 , Cnr , CYb , Cnb, V0 , b))
-print ("-----------------------------------------------")
-print("Dimension-having eigenvalues for short period are:" , short_period_d(muc , KY2 , CZa , Cmadot, Cmq, Cma , V0 , c))
-print()
-print("Dimension-having eigenvalues for phugoid are:" , phugoid_d(muc, CZa, Cmq, Cma,CXu , Cmu, CXa, CZu , CZ0, V0 , c))
-print()
-print("Dimension-having eigenvalues for Dutch roll are:" , dutch_roll_d(mub , KZ2 , Cnr , CYb , Cnb , V0 , b))
-
 
