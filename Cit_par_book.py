7181a0ed26d5c694eb22f5a489dbff9a4de8e059
axert5 <36628670+axert5@users.noreply.github.com>
2018-03-18 17:50:06 +0100

    fix

--- Cit_par_book.py
+++ Cit_par_book.py
@@ -23,14 +23,18 @@ KY2    = 0.980
 
 CL = 1.1360            
 
+
+c = 2.022
+b = 13.36
+
 # Stabiblity derivatives
 
-CX0    = 0
+CX0    = 0.
 CXu    = -0.2199
 CXa    = 0.4653
-CXadot = 0
-CXq    = 0
-CXde   = 0
+CXadot = 0.
+CXq    = 0.
+CXde   = 0.
 
 CZ0    = -1.1360
 CZu    = -2.2720
@@ -39,7 +43,7 @@ CZadot = -1.4300
 CZq    = -3.86
 CZde   = -0.6238
 
-Cmu    = 0
+Cmu    = 0.
 Cmadot = -3.7
 Cmq    = -7.04
 
@@ -47,7 +51,7 @@ CYb    = -0.9896
 CYbdot =  0     
 CYp    = -0.0870
 CYr    = +0.4300
-CYda   = -0.0
+CYda   = 0.0
 CYdr   = +0.3037
 
 Clb    = -0.0772
@@ -57,12 +61,11 @@ Clda   = -0.2349
 Cldr   = +0.0286
 
 Cnb    =  +0.1638
-Cnbdot =   0     
+Cnbdot =   0.     
 Cnp    =  -0.0108
 Cnr    =  -0.1930
 Cnda   =  +0.0286
 Cndr   =  -0.1261
 
-c = 2.022
-b = 13.36
+
 
